70a2efa7395fe129ebd64d4e3216b363f6ec9acd
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2024-01-25 16:42:17 +0800

    fix

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,7 +2,7 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async throws in
-        guard let token = ProcessInfo.processInfo.environment["TOKEN"] else { throw Abort(.badRequest) }
+        guard let token = Environment.get("TOKEN") else { throw Abort(.badRequest) }
         let events = try await Notion2ICS(token: token).fetchEvents()
         let ics = icsFromEventList(events)
         return ics.asString()
